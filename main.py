3304eab03a374ddc9efae65d0d94c93af204e12a
Mike <mikejhaze@gmail.com>
2022-06-26 14:33:47 -0400

    init commit

---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+MVP:
+This project will show my last ten ranked games with my placement. There will be a refresh
+button that I can hit whenever.
+
+Stretch Goals:
+- This app will show the comp and characters played
+- Not just me, enter username
+
+"""
+
+
+
+
+
